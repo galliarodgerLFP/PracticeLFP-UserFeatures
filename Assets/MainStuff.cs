936b15f9f471a8a2a604680c16d65297646cf9b4
galliarodgerLFP <74176429+galliarodgerLFP@users.noreply.github.com>
2020-11-13 10:54:21 +0200

    Add files via upload
    Assets folder

---
+++ Assets/MainStuff.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Net;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainStuff : MonoBehaviour
+{
+
+    public static MainStuff Instance;
+    public WebStuff Web;
+    
+
+    public GameObject viewsCreate;
+    public GameObject viewsLogin;
+    public GameObject viewsHome;
+    public GameObject viewsFIN;
+
+    public Text usern;
+    public Text passw;
+    public Text extras;
+
+
+
+    void Start()
+    {
+        SetHomeActive();
+
+        Instance = this;
+        Web = GetComponent<WebStuff>();
+ 
+    }
+
+    public void SetFINActive()
+    {
+        viewsHome.SetActive(false);
+        viewsCreate.SetActive(false);
+        viewsLogin.SetActive(false);
+        viewsFIN.SetActive(true);
+        
+  
+        
+
+    }
+
+    public void SetLoginActive()
+    {
+        viewsHome.SetActive(false);
+        viewsCreate.SetActive(false);
+        viewsFIN.SetActive(false);
+        viewsLogin.SetActive(true);
+    }
+    public void SetRegisterActive()
+    {
+        viewsHome.SetActive(false);
+        viewsCreate.SetActive(true);
+        viewsLogin.SetActive(false);
+        viewsFIN.SetActive(false);
+
+    }
+    public void SetHomeActive()
+    {
+        viewsHome.SetActive(true);
+        viewsCreate.SetActive(false);
+        viewsLogin.SetActive(false);
+        viewsFIN.SetActive(false);
+
+    }
+
+    
+}
